5406056f273b3d388028e64defaf4bb009d337de
WilliamConnatser <WilliamConnatser@users.noreply.github.com>
2019-05-07 13:58:32 -0500

    implemented changes in lesson

--- api/server.js
+++ api/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const helmet = require('helmet');
 const cors = require('cors');
+const session = require('express-session');
+
+const sessionConfig = {
+  name: 'monster', //defaults to sid
+  secret: 'my super secret key is secret',
+  cookie: {
+    httpOnly: true, //True if no SSL
+    maxAge: 1000 * 60 * 2, //milliseconds how long the cookie is valid for
+    secure: false
+  },
+  resave: false, //Resave session if it didn't change
+  saveUninitialized: true //create new session automatically
+}
 
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
 
 const server = express();
 
+server.use(session(sessionConfig))
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
@@ -15,7 +29,8 @@ server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
 
 server.get('/', (req, res) => {
-  res.send("It's alive!");
+  const username = req.session.username || 'stranger';
+  res.send(`Hi ${username}`);
 });
 
 module.exports = server;
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const bcrypt = require('bcryptjs');
-
+const restricted = require('./restricted-middleware');
 const Users = require('../users/users-model.js');
 
 // for endpoints beginning with /api/auth
@@ -19,22 +19,46 @@ router.post('/register', (req, res) => {
 });
 
 router.post('/login', (req, res) => {
-  let { username, password } = req.body;
+  let {
+    username,
+    password
+  } = req.body;
 
-  Users.findBy({ username })
+  Users.findBy({
+      username
+    })
     .first()
     .then(user => {
       if (user && bcrypt.compareSync(password, user.password)) {
+        //Set username in session
+        req.session.username = user.username;
+        //The cookie is set by express-session library when the response is send
         res.status(200).json({
-          message: `Welcome ${user.username}!`,
+          message: `Welcome ${user.username}! You got cookie`,
         });
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({
+          message: 'Invalid Credentials'
+        });
       }
     })
     .catch(error => {
       res.status(500).json(error);
     });
+});
+
+//Route protected by cookie
+router.get('/logout', restricted, (req, res) => {
+  if (req.session) {
+    //Destroy session- function provided by express-session
+    req.session.destroy(err => {
+      if (err) {
+        res.send('error logging out')
+      } else {
+        res.send('buh bye')
+      }
+    });
+  }
 });
 
 module.exports = router;
